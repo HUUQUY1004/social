2923441f5e871efb9cd30664409a6de94cad87a4
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2025-03-10 01:05:40 +0700

    Feat:Login account

--- Social/Social/src/main/java/com/social/Social/service/CustomerUserDetailsService.java
+++ Social/Social/src/main/java/com/social/Social/service/CustomerUserDetailsService.java
@@ -1,14 +1,27 @@
 package com.social.Social.service;
 
+import com.social.Social.model.User;
+import com.social.Social.responsitory.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+
 @Service
 public class CustomerUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository.findByEmail(username);
+        if(user == null){
+            throw  new UsernameNotFoundException("User not found with email " + username);
+
+        }
+        return  new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
     }
 }
