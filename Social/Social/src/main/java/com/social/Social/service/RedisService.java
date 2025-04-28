68d4b8caddef78c3219b3d81d364d779a77e0eb0
HUUQUY1004 <99793142+HUUQUY1004@users.noreply.github.com>
2025-04-28 19:49:08 +0700

    Redis Config

---
+++ Social/Social/src/main/java/com/social/Social/service/RedisService.java
@@ -0,0 +1,19 @@
+package com.social.Social.service;
+
+public class RedisService implements  RedisServiceImp{
+    @Override
+    public void saveData(String key, Object value) {
+
+
+    }
+
+    @Override
+    public void getData(String key) {
+
+    }
+
+    @Override
+    public void deleteData(String key) {
+
+    }
+}
